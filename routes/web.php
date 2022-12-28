dedca1daf14219d695f5913998b63bf11113e45a
mike <mikelionardheredia@gmail.com>
2022-12-28 10:05:00 -0400

    V.1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Estaciones;
+use App\Http\Controllers\EstacionesController;
+
+use function GuzzleHttp\Promise\all;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +24,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('/estaciones', EstacionesController::class);
