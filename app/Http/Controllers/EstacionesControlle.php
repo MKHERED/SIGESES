20484b20954c5f24e1af585d32c6fe51abfdaec0
mike <mikelionardheredia@gmail.com>
2023-06-14 17:03:44 -0400

    se termino el update
    ya se suben los documentos, solo falta hacer el sistema de eliminacion

--- app/Http/Controllers/EstacionesControlle.php
+++ app/Http/Controllers/EstacionesControlle.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Details;
 use App\Models\Estaciones;
+use App\Models\Link_doc;
 use Illuminate\Contracts\Session\Session;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -23,26 +24,25 @@ class EstacionesControlle extends Controller
 
         foreach (Estaciones::all() as $estacion) {
             $estaciones[] = $estacion;
-            $estadosList = ["Seleccione un Estado", "Amazonas", "Anzoátegui",
-            "Apure", "Aragua", "Barinas", "Bolívar", "Carabobo",
-            "Cojedes", "Delta Amacuro", "Dependencias Federales",
-            "Distrito Federal", "Falcón", "Guárico", "Lara", "Mérida",
-            "Miranda", "Monagas", "Nueva Esparta", "Portuguesa", "Sucre",
-            "Táchira", "Trujillo", "Vargas", "Yaracuy", "Zulia"];
-            $regionList = ["Occidente", "Centro", "Oriente" ];
-            
+            $estadosList = [
+                "Seleccione un Estado", "Amazonas", "Anzoátegui",
+                "Apure", "Aragua", "Barinas", "Bolívar", "Carabobo",
+                "Cojedes", "Delta Amacuro", "Dependencias Federales",
+                "Distrito Federal", "Falcón", "Guárico", "Lara", "Mérida",
+                "Miranda", "Monagas", "Nueva Esparta", "Portuguesa", "Sucre",
+                "Táchira", "Trujillo", "Vargas", "Yaracuy", "Zulia"
+            ];
+            $regionList = ["Occidente", "Centro", "Oriente"];
+
             $estacion->estado = $estadosList[$estacion['estado']];
             $estacion->region = $regionList[$estacion['region']];
         }
-        if ($estaciones==[]) {
+        if ($estaciones == []) {
             $estaciones = [];
             //echo $estaciones;
         }
 
         return view('estaciones.index', compact('estaciones'));
-        
- 
-        
     }
 
     /**
@@ -62,27 +62,25 @@ class EstacionesControlle extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {       
-        
+    {
+
         $estaciones = new Estaciones;
-        
+
         $estaciones->id = $request->id;
         $estaciones->siglas = $request->siglas;
         $estaciones->nombre = $request->nombre;
         $estaciones->ubicacion = $request->ubicacion;
-        
-        if ($request->gms == "on"){
+
+        if ($request->gms == "on") {
             //                             GRADOS                   Minutos                        Segundos
             $estaciones->longitud = $request->longitud . "º " . $request->longitud1 . '" ' . $request->longitud2 . "'";
             //$estaciones->longitud = $request->longitud;
-            $estaciones->latitud = $request->latitud. "º ". $request->latitud1 . '" ' . $request->latitud2 . "'";
+            $estaciones->latitud = $request->latitud . "º " . $request->latitud1 . '" ' . $request->latitud2 . "'";
             //$estaciones->latitud = $request->latitud;
         } else {
             $estaciones->longitud = $request->longitud;
-            $estaciones->latitud = $request->latitud;            
-        
-        
-        } 
+            $estaciones->latitud = $request->latitud;
+        }
 
         $estaciones->altitud = $request->altitud;
         $estaciones->region = $request->region;
@@ -90,12 +88,12 @@ class EstacionesControlle extends Controller
         $estaciones->operativa = $request->operativa;
         $estaciones->imagen_n = "sin contenido";
         $estaciones->img_dir = "sin contenido";
-        
+
         //return response()->json($estaciones);
         //guardado de la imagen
         if ($request->hasFile('imagen_n')) {
-            $estaciones->imagen_n=$request->file('imagen_n')->store('uploads/'.$estaciones["nombre"].'/', 'public');
-            $estaciones->img_dir=$estaciones['imagen_n'];
+            $estaciones->imagen_n = $request->file('imagen_n')->store('uploads/' . $estaciones["nombre"] . '/', 'public');
+            $estaciones->img_dir = $estaciones['imagen_n'];
         } else {
             return response()->json($estaciones);
         }
@@ -104,29 +102,28 @@ class EstacionesControlle extends Controller
 
         $doc = $request->doc;
 
-        if ($doc == null ) {
+        if ($doc == null) {
             // Si subir es igual a null
 
             $estaciones->save();
 
             $estacion = Estaciones::findOrFail($estaciones['id']);
-            $id = "/".$estacion['id'];
+            $id = "/" . $estacion['id'];
             //$data = [$id, $doc];
 
-            return redirect()->route("details.index", $id)->with(["mensaje"=>"La estación ".$estacion['nombre']." creada con exito"]);
+            return redirect()->route("details.index", $id)->with(["mensaje" => "La estación " . $estacion['nombre'] . " creada con exito"]);
         } elseif ($doc == "on") {
             // Si se va a subir archivo
 
             $estaciones->save();
 
             $estacion = Estaciones::findOrFail($estaciones['id']);
-            $id = "/".$estacion['id'];
+            $id = "/" . $estacion['id'];
             $data = [$id, $doc];
 
-            return redirect()->route("details.index", $data)->with(["mensaje"=>"La estación ".$estacion['nombre']." creada con exito"]);
+            return redirect()->route("details.index", $data)->with(["mensaje" => "La estación " . $estacion['nombre'] . " creada con exito"]);
             // --------------------------------------------------
         }
-
     }
 
     /**
@@ -139,26 +136,33 @@ class EstacionesControlle extends Controller
     {
         $estacion = Estaciones::findOrFail($id);
         $detail = Details::findOrFail($id);
+        $link_docs = DB::table('link_docs')->where('id_estacion', $id)->get();
+
+
         if ($detail == null) {
             $detail = 'Sin resultados';
         }
 
-        $estadosList = ["Seleccione un Estado", "Amazonas", "Anzoátegui",
-        "Apure", "Aragua", "Barinas", "Bolívar", "Carabobo",
-        "Cojedes", "Delta Amacuro", "Dependencias Federales",
-        "Distrito Federal", "Falcón", "Guárico", "Lara", "Mérida",
-        "Miranda", "Monagas", "Nueva Esparta", "Portuguesa", "Sucre",
-        "Táchira", "Trujillo", "Vargas", "Yaracuy", "Zulia"];
-        $regionList = ["Occidente", "Centro", "Oriente" ];
-        $operativaList = ["No", "Si" ];
+        $estadosList = [
+            "Seleccione un Estado", "Amazonas", "Anzoátegui",
+            "Apure", "Aragua", "Barinas", "Bolívar", "Carabobo",
+            "Cojedes", "Delta Amacuro", "Dependencias Federales",
+            "Distrito Federal", "Falcón", "Guárico", "Lara", "Mérida",
+            "Miranda", "Monagas", "Nueva Esparta", "Portuguesa", "Sucre",
+            "Táchira", "Trujillo", "Vargas", "Yaracuy", "Zulia"
+        ];
+        $regionList = ["Occidente", "Centro", "Oriente"];
+        $operativaList = ["No", "Si"];
 
         $estacion->estado = $estadosList[$estacion['estado']];
         $estacion->region = $regionList[$estacion['region']];
         $estacion->operativa = $operativaList[$estacion['operativa']];
+        
 
         // crear un array completo de todos los datos
-        return view('estaciones.show',compact('estacion', 'detail')); 
-        //response()->json($estacion);
+        return view('estaciones.show', compact('estacion', 'detail', 'link_docs'));
+
+        //response()->json(print_r($link_docs));
         //redirect()->route('estaciones.show', $estacion);
         //view('estaciones.show', $estacion);
     }
@@ -175,18 +179,18 @@ class EstacionesControlle extends Controller
         $estaciones = Estaciones::findOrFail($id);
 
         $sigla = $estaciones['siglas'];
-        
+
         $details = DB::table('details')->where('siglas', $sigla)->first();
 
-         if ($details) {
-             // cambiar componente
-             $validador = 'cambiar';
-         } else {
-             // agregar los componentes
-             $validador = 'agregar';
-         }
+        if ($details) {
+            // cambiar componente
+            $validador = 'cambiar';
+        } else {
+            // agregar los componentes
+            $validador = 'agregar';
+        }
 
-        
+
 
         return view('estaciones.edit', compact('estaciones', 'validador'));
     }
@@ -199,8 +203,8 @@ class EstacionesControlle extends Controller
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
-    {   
-        $estacion = request()->except(['_token', '_method']);      
+    {
+        $estacion = request()->except(['_token', '_method']);
 
         // Ordenar aqui y poner la URL donde se guarda la imagen
         $imagen_n = $request->imagen_n;
@@ -210,24 +214,24 @@ class EstacionesControlle extends Controller
         //guardado de la imagen
         if ($request->hasFile('imagen_n')) {
             $db = Estaciones::findOrFail($id);
-            Storage::delete('public/'.$db->img_dir);
+            Storage::delete('public/' . $db->img_dir);
 
-            $estacion['imagen_n']=$request->file('imagen_n')->store('uploads/'.$estacion["nombre"].'/', 'public');
-            $estacion['img_dir']=$estacion['imagen_n'];
+            $estacion['imagen_n'] = $request->file('imagen_n')->store('uploads/' . $estacion["nombre"] . '/', 'public');
+            $estacion['img_dir'] = $estacion['imagen_n'];
         }
-        
-     
 
-        
-        
+
+
+
+
         Estaciones::where('id', '=', $id)->update($estacion);
 
 
 
-        return redirect()->route("estaciones.index")->with(["mensaje" => "Estación actualizada",]);//response()->json($estacion);//
+        return redirect()->route("estaciones.index")->with(["mensaje" => "Estación actualizada",]); //response()->json($estacion);//
 
 
-    
+
     }
 
     /**
@@ -239,25 +243,25 @@ class EstacionesControlle extends Controller
     public function destroy($id)
     {
         $db = Estaciones::findOrFail($id);
-        Storage::delete('public/'.$db->img_dir);
-        Storage::delete('public/uploads/'.$db->nombre);
-        
+        Storage::delete('public/' . $db->img_dir);
+        Storage::delete('public/uploads/' . $db->nombre);
+
         Estaciones::destroy($id);
         return redirect()->route("estaciones.index")->with(["mensaje" => "Estación Eliminada con exito",]);
     }
 
     public function mapa()
     {
-        return view('estaciones/mapa'); 
+        return view('estaciones/mapa');
     }
 
     // public function vistaDoc($id)
     // {   
     //     return view('estaciones/updateDoc');
     // }
-    public function updateDoc(Request $request, $id){
+    public function updateDoc(Request $request, $id)
+    {
         // verificar el $id
         Estaciones::where('id', '=', $id);
-
     }
 }
---
+++ database/migrations/2023_06_14_193732_create_link_docs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('link_docs', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('id_estacion');
+            $table->string('nombre_estacion');
+            $table->string('direccion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('link_docs');
+    }
+};
