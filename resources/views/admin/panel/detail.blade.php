0801ecb69370344d7b5137a18fa4c7c8b59a7a54
mike <mikelionardheredia@gmail.com>
2023-07-10 19:17:37 -0400

    avance de details
    se creo y se termino el boton para eliminar los componentes de cada estacion

--- resources/views/admin/panel/detail.blade.php
+++ resources/views/admin/panel/detail.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 <div class="table-responsive">
+    <script src="{{asset('js/gestion.js') }}"></script>
     <form action="{{route('panel.detail')}}" method="get">
     @csrf
     <div class="row text-left justify-content-center">
@@ -70,63 +71,167 @@
         </tr>
         <tr>
             <td>Antena gps</td>
-            <td>{{$detail->antena_gps}}</td>
+            <td id="inp_antena_gps{{$detail->id}}">{{$detail->antena_gps}}</td>
             <td>{{$detail->instalacion_satelital}}</td>
             <td>Autor aun por hacer</td>
+            <td>
+            @if (Auth::user()->tipo_usuario)
+                <div class="btn-group me-2">
+                    <form class="" action="{{ route('details.destroy', $detail->id) }}" method="POST">
+                            @csrf
+                            {{ method_field('DELETE') }}
+                            <input type="submit" class="btn btn-sm btn-outline-dark text-danger" type="submit" onclick="return confirm('¿Quieres borrar? {{$detail->nombre}}' )" value="Borrar">
+                            
+                            <input type="text" name="component" value="antenagps" hidden>
+                    
+                    </form>
+                </div>
+            @endif
+            </td>
         </tr>
         <tr>
             <td>Antena Parabolica</td>
             <td>{{$detail->antena_parabolica}}</td>
             <td>{{$detail->instalacion_satelital}}</td>
             <td>Autor</td>
+            <td>
+            @if (Auth::user()->tipo_usuario)
+                <form action="{{ route('details.destroy', $detail->id) }}" method="POST">
+                        @csrf
+                        {{ method_field('DELETE') }}
+                        <input type="submit" class="btn btn-sm btn-outline-secondary text-danger" type="submit" onclick="return confirm('¿Quieres borrar? {{$detail->nombre}}' )" value="Borrar">
+                        <input type="text" name="component" value="antenaparabolica" hidden>
+                </form>
+            @endif
+            </td>
         </tr>
         <tr>
             <td>Bateria</td>
             <td>{{$detail->bateria}}</td>
             <td>{{$detail->instalacion_satelital}}</td>
             <td>Autor</td>
+            <td>
+            @if (Auth::user()->tipo_usuario)
+                <form action="{{ route('details.destroy', $detail->id) }}" method="POST">
+                        @csrf
+                        {{ method_field('DELETE') }}
+                        <input type="submit" class="btn btn-sm btn-outline-secondary text-danger" type="submit" onclick="return confirm('¿Quieres borrar? {{$detail->nombre}}' )" value="Borrar">
+                        <input type="text" name="component" value="bateria" hidden>
+                </form>
+            @endif
+            </td>
         </tr>
         <tr>
             <td>Controlador de carga</td>
             <td>{{$detail->controlador_carga}}</td>
             <td>{{$detail->instalacion_satelital}}</td>
-            <td>Autor</td>
+            <td>Autor</td>            
+            <td>
+            @if (Auth::user()->tipo_usuario)
+                <form action="{{ route('details.destroy', $detail->id) }}" method="POST">
+                        @csrf
+                        {{ method_field('DELETE') }}
+                        <input type="submit" class="btn btn-sm btn-outline-secondary text-danger" type="submit" onclick="return confirm('¿Quieres borrar? {{$detail->nombre}}' )" value="Borrar">
+                        <input type="text" name="component" value="controladorcarga" hidden>
+                </form>
+            @endif
+            </td>
         </tr>
         <tr>
             <td>Digitalizador</td>
             <td>{{$detail->digitalizador}}</td>
             <td>{{$detail->instalacion_satelital}}</td>
             <td>Autor</td>
+            <td>
+            @if (Auth::user()->tipo_usuario)
+                <form action="{{ route('details.destroy', $detail->id) }}" method="POST">
+                        @csrf
+                        {{ method_field('DELETE') }}
+                        <input type="submit" class="btn btn-sm btn-outline-secondary text-danger" type="submit" onclick="return confirm('¿Quieres borrar? {{$detail->nombre}}' )" value="Borrar">
+                        <input type="text" name="component" value="digitalizador" hidden>
+                </form>
+            @endif
+            </td>
         </tr>
         <tr>
             <td>Modem</td>
             <td>{{$detail->modem_satelital}}</td>
             <td>{{$detail->instalacion_satelital}}</td>
             <td>Autor</td>
+            <td>
+            @if (Auth::user()->tipo_usuario)
+                <form action="{{ route('details.destroy', $detail->id) }}" method="POST">
+                        @csrf
+                        {{ method_field('DELETE') }}
+                        <input type="submit" class="btn btn-sm btn-outline-secondary text-danger" type="submit" onclick="return confirm('¿Quieres borrar? {{$detail->nombre}}' )" value="Borrar">
+                        <input type="text" name="component" value="modemsatelital" hidden>
+                </form>
+            @endif
+            </td>
         </tr>
         <tr>
             <td>Panel solar</td>
             <td>{{$detail->panel_solar}}</td>
             <td>{{$detail->instalacion_satelital}}</td>
             <td>Autor</td>
+            <td>
+            @if (Auth::user()->tipo_usuario)
+                <form action="{{ route('details.destroy', $detail->id) }}" method="POST">
+                        @csrf
+                        {{ method_field('DELETE') }}
+                        <input type="submit" class="btn btn-sm btn-outline-secondary text-danger" type="submit" onclick="return confirm('¿Quieres borrar? {{$detail->nombre}}' )" value="Borrar">
+                        <input type="text" name="component" value="panelsolar" hidden>
+                </form>
+            @endif
+            </td>
         </tr>
         <tr>
             <td>Regulador de carga</td>
             <td>{{$detail->regulador_carga}}</td>
             <td>{{$detail->instalacion_satelital}}</td>
             <td>Autor</td>
+            <td>
+            @if (Auth::user()->tipo_usuario)
+                <form action="{{ route('details.destroy', $detail->id) }}" method="POST">
+                        @csrf
+                        {{ method_field('DELETE') }}
+                        <input type="submit" class="btn btn-sm btn-outline-secondary text-danger" type="submit" onclick="return confirm('¿Quieres borrar? {{$detail->nombre}}' )" value="Borrar">
+                        <input type="text" name="component" value="reguladorcarga" hidden>
+                </form>
+            @endif
+            </td>
         </tr>
         <tr>
             <td>Sismometro</td>
             <td>{{$detail->sismometro}}</td>
             <td>{{$detail->instalacion_satelital}}</td>
             <td>Autor</td>
+            <td>
+            @if (Auth::user()->tipo_usuario)
+                <form action="{{ route('details.destroy', $detail->id) }}" method="POST">
+                        @csrf
+                        {{ method_field('DELETE') }}
+                        <input type="submit" class="btn btn-sm btn-outline-secondary text-danger" type="submit" onclick="return confirm('¿Quieres borrar? {{$detail->nombre}}' )" value="Borrar">
+                        <input type="text" name="component" value="sismometro" hidden>
+                </form>
+            @endif
+            </td>
         </tr>
         <tr>
             <td>Trompeta</td>
             <td>{{$detail->trompeta_satelital}}</td>
             <td>{{$detail->instalacion_satelital}}</td>
             <td>Autor</td>
+            <td>
+            @if (Auth::user()->tipo_usuario)
+                <form action="{{ route('details.destroy', $detail->id) }}" method="POST">
+                        @csrf
+                        {{ method_field('DELETE') }}
+                        <input type="submit" class="btn btn-sm btn-outline-secondary text-danger" type="submit" onclick="return confirm('¿Quieres borrar? {{$detail->nombre}}' )" value="Borrar">
+                        <input type="text" name="component" value="trompetasatelital" hidden>
+                </form>
+            @endif
+            </td>
         </tr>
 
     </tbody>    
